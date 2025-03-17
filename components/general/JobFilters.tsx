aac895ae0997721ccbb66d57f1ab9235bdee0f3e
Liban Yonis <assadyonis@gmail.com>
2025-03-17 17:17:49 +0300

    fix the filter issue in searchparams

--- components/general/JobFilters.tsx
+++ components/general/JobFilters.tsx
@@ -63,6 +63,8 @@ export function JobFilter() {
 
     const newValue = Array.from(current).join(",");
 
+    console.log(newValue);
+
     router.push(`?${createQueryString("jobTypes", newValue)}`);
   }
 
